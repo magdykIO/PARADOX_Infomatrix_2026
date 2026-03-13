712c3cb5bc6dc07cb26ac9659e88f7e39728e3f3
shliMaks <maksymko.letter@gmail.com>
2026-03-14 00:38:39 +0200

    Added columns for users and quests

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, DateTime, Boolean, ForeignKey
+from sqlalchemy import Column, Integer, String, DateTime, Boolean, ForeignKey, JSON
 from sqlalchemy.sql import func
 from sqlalchemy.orm import relationship
 from database import Base
@@ -8,17 +8,20 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, unique=True, index=True)
-
-    full_name = Column(String, unique=False, nullable=False)
+    full_name = Column(String, nullable=False)
     username = Column(String, unique=True, index=True, nullable=False)
     email = Column(String, unique=True, nullable=False)
-
     hashed_password = Column(String, nullable=False)
 
-    last_seen = Column(DateTime, onupdate=func.now())  # When user's info changes in db
-    join_time = Column(DateTime, server_default=func.now())  # When user signed up
+    nuts_amount = Column(Integer, default=0)
+    goal = Column(String)
+    current_skin = Column(String, default="regular")
+    unlocked_skins = Column(JSON, default=list)
+    # Зберігаємо список імен скінів ["regular", "cool_squirrel"]
 
-    # Зв'язок з квестами: один юзер має багато квестів
+    last_seen = Column(DateTime, onupdate=func.now())
+    join_time = Column(DateTime, server_default=func.now())
+
     quests = relationship("Quest", back_populates="owner", cascade="all, delete-orphan")
 
 
@@ -26,12 +29,31 @@ class Quest(Base):
     __tablename__ = "quests"
 
     id = Column(Integer, primary_key=True, index=True)
-    title = Column(String, nullable=False)  # Назва таски
-    category = Column(String)  # Daily, Learn Matan, History тощо
-    nuts_reward = Column(Integer)  # Кількість горішків (+1 на фото)
+    title = Column(String, nullable=False)
+    description = Column(String)
+    category = Column(String)
+    from_who = Column(String)  # Від кого таска
 
-    is_completed = Column(Boolean, default=False)  # Виконано чи ні
-    deadline = Column(DateTime, nullable=True)  # Коли таска згорає
+    difficulty = Column(Integer)
+    base_reward = Column(Integer)
+
+    # Конфіги від Gemini
+    # можна зберігати декілька для різної важкості
+    deadlines_config = Column(JSON, nullable=True)
+    multipliers_config = Column(JSON, nullable=True)
+
+    # "easy", "medium" або "hard"
+    selected_difficulty_level = Column(String)
+
+    # Розрахований виграш (base_reward * multiplier).
+    # Зберігаємо тут, щоб не рахувати кожен раз.
+    final_reward = Column(Integer, nullable=True)
+
+    is_completed = Column(Boolean, default=False)
+
+    # Дата закінчення дедлайну (вибрана з config або встановлена вручну)
+    deadline = Column(DateTime, nullable=True)
+
     created_at = Column(DateTime, server_default=func.now())
 
     user_id = Column(Integer, ForeignKey("users.id"))
